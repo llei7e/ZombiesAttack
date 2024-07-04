0be43d3da03518166ce1a4a11b694511452c4eaa
Giovani-RodriguesS <giovanirodrigues021@gmail.com>
2024-07-04 14:51:05 -0300

    feat: mediaplayer created in sounds

--- ZombiesAttack/src/main/java/clueless/zombiesattack/Sounds.java
+++ ZombiesAttack/src/main/java/clueless/zombiesattack/Sounds.java
@@ -7,12 +7,15 @@ import java.io.File;
 public class Sounds {
 
     // Properties
-    private final MediaPlayer sound;
+    private MediaPlayer sound;
 
     // Construtor
+    public Sounds () {
+
+    }
+
     public Sounds(String audioPath) {
-        // sounds effect
-        this.sound = new MediaPlayer(new Media(new File(audioPath).toURI().toString()));
+        this.sound = new MediaPlayer(new Media(new File (audioPath).toURI().toString()));
     }
 
 
@@ -20,4 +23,9 @@ public class Sounds {
     public MediaPlayer getSoundEffect() {
         return sound;
     }
+
+    public static MediaPlayer getRifleSound() {
+        return new MediaPlayer(new Media(new File (/*caminho*/).toURI().toString()));
+    }
 }
+
